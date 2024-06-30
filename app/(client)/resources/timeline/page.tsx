5804b1b3417bd9e8a236aa54c416890049dae3a5
Aleshere <alessioburgio85@gmail.com>
2024-06-29 23:32:03 -0400

    Escaped quotes

--- app/(client)/resources/timeline/page.tsx
+++ app/(client)/resources/timeline/page.tsx
@@ -29,9 +29,9 @@ const page = async () => {
       <Header title="Timeline" />
       
       <p>
-        The origins of NextGen trace back to the "Vision 100 - A Century Of Aviation Reauthorization Act," passed by Congress in December 2003. This bill aimed to shift from radar-based guidance systems to GPS positioning, allowing for more direct flight routes to save fuel, time, money, and - allegedly - reduce carbon emissions. In practice, NextGen intended to increase airport throughput by guiding aircraft more precisely into airports, benefiting airlines with fuel savings and higher profits.
+        The origins of NextGen trace back to the &quot;Vision 100 - A Century Of Aviation Reauthorization Act,&quot; passed by Congress in December 2003. This bill aimed to shift from radar-based guidance systems to GPS positioning, allowing for more direct flight routes to save fuel, time, money, and - allegedly - reduce carbon emissions. In practice, NextGen intended to increase airport throughput by guiding aircraft more precisely into airports, benefiting airlines with fuel savings and higher profits.
         <hr className="mt-6 mb-6" />
-        However, achieving these goals required aircraft to fly at significantly lower elevations in congested airspace, resulting in a dramatic increase in low-altitude flights over communities. This shift has led to a significant rise in noise and air pollution for many communities, contradicting the FAA's assurances of "no new or significant" impacts from NextGen.
+        However, achieving these goals required aircraft to fly at significantly lower elevations in congested airspace, resulting in a dramatic increase in low-altitude flights over communities. This shift has led to a significant rise in noise and air pollution for many communities, contradicting the FAA&apos;s assurances of &quot;no new or significant&quot; impacts from NextGen.
         <hr className="mt-6 mb-6" />
         Promises of decreased carbon emissions from more efficient routing have not materialized as expected, with studies showing minimal fuel savings and a net increase in emissions due to the overall goal of increasing air traffic.
         <hr className="mt-6 mb-6" />
